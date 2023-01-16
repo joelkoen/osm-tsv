51f285920dfe9096421e498b8cca66d001b0758e
Joel Koen <mail@joelkoen.com>
2023-01-16 16:50:14 +1000

    feat: load addresses into josm

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            (pkgs.python3.withPackages (ps: with ps; [
+              black
+              gdal
+              jupyterlab
+              ipython
+              tokenize-rt
+              shapely
+            ]))
+            pkgs.josm
+          ];
+        };
+      });
+}
